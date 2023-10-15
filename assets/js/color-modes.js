7c2849994bf667e0659087c6ec9f7a616416f018
Rishi Kataria <rkat0008@student.monash.edu>
2023-10-15 12:07:03 +1100

    Changed theme.

--- assets/js/color-modes.js
+++ assets/js/color-modes.js
@@ -16,14 +16,14 @@
       return storedTheme
     }
 
-    return window.matchMedia('(prefers-color-scheme: light)').matches ? 'dark' : 'light'
+    return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'light' : 'light'
   }
 
   const setTheme = theme => {
     if (theme === 'auto' && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-      document.documentElement.setAttribute('data-bs-theme', 'dark')
+      document.documentElement.setAttribute('data-bs-theme', 'light')
     } else {
-      document.documentElement.setAttribute('data-bs-theme', theme)
+      document.documentElement.setAttribute('data-bs-theme', 'light')
     }
   }
 
